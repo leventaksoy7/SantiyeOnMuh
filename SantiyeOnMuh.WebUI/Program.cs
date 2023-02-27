3abeb662b59a1f8de8c8e5d716e0ff99d69f0b68
blcaksoy@gmail.com <blcaksoy@gmail.com>
2023-02-27 17:45:50 +0300

    UfakTefekDüzeltmeler

--- SantiyeOnMuh.WebUI/Program.cs
+++ SantiyeOnMuh.WebUI/Program.cs
@@ -18,8 +18,43 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddMemoryCache();
 
 // Add a custom scoped service.
+//SANTÝYE
 builder.Services.AddScoped<ISantiyeService, SantiyeManager>();
 builder.Services.AddScoped<ISantiyeRepository, EfCoreSantiyeRepository>();
+
+builder.Services.AddScoped<ISantiyeKasaRepository, EfCoreSantiyeKasaRepository>();
+builder.Services.AddScoped<ISantiyeKasaService, SantiyeKasaManager>();
+
+builder.Services.AddScoped<ISantiyeGiderKalemiRepository, EfCoreSantiyeGiderKalemiRepository>();
+builder.Services.AddScoped<ISantiyeGiderKalemiService, SantiyeGiderKalemiManager>();
+
+//CARÝ
+builder.Services.AddScoped<ICariHesapRepository, EfCoreCariHesapRepository>();
+builder.Services.AddScoped<ICariHesapService, CariHesapManager>();
+
+builder.Services.AddScoped<ICariKasaRepository, EfCoreCariKasaRepository>();
+builder.Services.AddScoped<ICariKasaService, CariKasaManager>();
+
+builder.Services.AddScoped<ICariGiderKalemiRepository, EfCoreCariGiderKalemiRepository>();
+builder.Services.AddScoped<ICariGiderKalemiService, CariGiderKalemiManager>();
+
+//BANKA
+builder.Services.AddScoped<IBankaHesapRepository, EfCoreBankaHesapRepository>();
+builder.Services.AddScoped<IBankaHesapService, BankaHesapManager>();
+
+builder.Services.AddScoped<IBankaKasaRepository, EfCoreBankaKasaRepository>();
+builder.Services.AddScoped<IBankaKasaService, BankaKasaManager>();
+
+//ODEME
+builder.Services.AddScoped<IOdemeCekRepository, EfCoreOdemeCekRepository>();
+builder.Services.AddScoped<IOdemeCekService, OdemeCekManager>();
+
+builder.Services.AddScoped<IOdemeNakitRepository, EfCoreOdemeNakitRepository>();
+builder.Services.AddScoped<IOdemeNakitService, OdemeNakitManager>();
+
+//SÝRKET
+builder.Services.AddScoped<ISirketRepository, EfCoreSirketRepository>();
+builder.Services.AddScoped<ISirketService, SirketManager>();
 
 var app = builder.Build();
 
