91c81fd99d1bd617c8bfb88b292b363be893d4d6
blcaksoy@gmail.com <blcaksoy@gmail.com>
2023-03-02 00:13:52 +0300

    RouterlarVeLayoutEklendi

--- SantiyeOnMuh.WebUI/Program.cs
+++ SantiyeOnMuh.WebUI/Program.cs
@@ -76,8 +76,290 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=AnaSayfa}/{action=AnaSayfa}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=AnaSayfa}/{action=AnaSayfa}/{id?}");
+
+    //INDEX
+    //EKLE
+    //GUNCELLE
+    //DETAY
+    //SÝL
+    #region ÞANTÝYE KASA
+    //INDEX
+    app.MapControllerRoute(
+        name: "SantiyeKasa",
+        pattern: "SantiyeKasa/SantiyeKasa/{santiyeid}/{gkid?}",
+        defaults: new { controller = "SantiyeKasa", action = "SantiyeKasa" }
+        );
+    app.MapControllerRoute(
+        name: "SantiyeKasaArsiv",
+        pattern: "SantiyeKasa/SantiyeKasaArsiv/{santiyeid}/{gkid?}",
+        defaults: new { controller = "SantiyeKasa", action = "SantiyeKasaArsiv" }
+        );
+    //EKLE
+    app.MapControllerRoute(
+        name: "SantiyeKasaEkleme",
+        pattern: "SantiyeKasa/SantiyeKasaEkleme",
+        defaults: new { controller = "SantiyeKasa", action = "SantiyeKasaEkleme" }
+        );
+    app.MapControllerRoute(
+        name: "SantiyeKasaEklemeFromSantiye",
+        pattern: "SantiyeKasa/SantiyeKasaFaturaEklemeFromSantiye/{santiyeid}",
+        defaults: new { controller = "SantiyeKasa", action = "SantiyeKasaEklemeFromSantiye" }
+        );
+    //GUNCELLE
+    app.MapControllerRoute(
+        name: "SantiyeKasaGuncelleFromSantiye",
+        pattern: "SantiyeKasa/SantiyeKasaGuncelleFromSantiye/{santiyekasaid?}",
+        defaults: new { controller = "SantiyeKasa", action = "SantiyeKasaGuncelleFromSantiye" }
+        );
+    //DETAY
+    app.MapControllerRoute(
+        name: "SantiyeKasaDetay",
+        pattern: "SantiyeKasa/SantiyeKasaDetay/{id}",
+        defaults: new { controller = "SantiyeKasa", action = "SantiyeKasaDetay" }
+        );
+    //SÝL
+    app.MapControllerRoute(
+        name: "SantiyeKasaSil",
+        pattern: "SantiyeKasa/SantiyeKasaSil/{santiyekasaid?}",
+        defaults: new { controller = "SantiyeKasa", action = "SantiyeKasaSil" }
+        );
+    #endregion
+
+    #region CARÝ HESAP
+    //INDEX
+    app.MapControllerRoute(
+        name: "CariHesap",
+        pattern: "CariHesap/CariHesap/{santiyeid?}",
+        defaults: new { controller = "CariHesap", action = "CariHesap" }
+        );
+    app.MapControllerRoute(
+        name: "CariHesapArsiv",
+        pattern: "CariHesap/CariHesapArsiv/{santiyeid?}",
+        defaults: new { controller = "CariHesap", action = "CariHesapArsiv" }
+        );
+    //EKLE
+    app.MapControllerRoute(
+        name: "CariHesapEkleme",
+        pattern: "CariHesap/CariHesapEkleme",
+        defaults: new { controller = "CariHesap", action = "CariHesapEkleme" }
+        );
+    //GUNCELLE
+    app.MapControllerRoute(
+        name: "CariHesapGuncelle",
+        pattern: "CariHesap/CariHesapGuncelle/{carihesapid?}",
+        defaults: new { controller = "CariHesap", action = "CariHesapGuncelle" }
+        );
+    //DETAY
+    //SÝL
+    app.MapControllerRoute(
+        name: "CariHesapSil",
+        pattern: "CariHesap/CariHesapSil/{carihesapid?}",
+        defaults: new { controller = "CariHesap", action = "CariHesapSil" }
+        );
+    #endregion
+
+    #region CARÝ KASA
+    //INDEX
+    app.MapControllerRoute(
+        name: "CariKasa",
+        pattern: "CariKasa/CariKasa/{carihesapid}/{gkid?}",
+        defaults: new { controller = "CariKasa", action = "CariKasa" }
+        );
+    //EKLE
+    app.MapControllerRoute(
+        name: "CariKasaEkleme",
+        pattern: "CariKasa/CariKasaEkleme",
+        defaults: new { controller = "CariKasa", action = "CariKasaEkleme" }
+        );
+    app.MapControllerRoute(
+        name: "CariKasaEklemeFromCari",
+        pattern: "CariKasa/CariKasaFaturaEklemeFromCari/{carihesapid}",
+        defaults: new { controller = "CariKasa", action = "CariKasaEklemeFromCari" }
+        );
+    //GUNCELLE
+    app.MapControllerRoute(
+        name: "CariKasaGuncelle",
+        pattern: "CariKasa/CariKasaGuncelle/{carikasaid?}",
+        defaults: new { controller = "CariKasa", action = "CariKasaGuncelle" }
+        );
+    app.MapControllerRoute(
+        name: "CariKasa",
+        pattern: "CariKasa/CariKasaFaturaGuncelleFromCari/{carikasaid?}",
+        defaults: new { controller = "CariKasa", action = "CariKasaFaturaGuncelleFromCari" }
+        );
+    //DETAY
+    app.MapControllerRoute(
+        name: "CariKasaDetay",
+        pattern: "CariKasa/CariKasaDetay/{carikasaid?}",
+        defaults: new { controller = "CariKasa", action = "CariKasaDetay" }
+        );
+    //SÝL
+    app.MapControllerRoute(
+        name: "CariKasaSil",
+        pattern: "CariKasa/CariKasaSil/{carikasaid?}",
+        defaults: new { controller = "CariKasa", action = "CariKasaSil" }
+        );
+
+
+
+
+
+
+
+    #endregion
+
+    #region BANKA KASA
+    //INDEX
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasa/{bankahesapid?}",
+        defaults: new { controller = "BankaKasa", action = "BankaKasa" }
+        );
+    //EKLE
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasaEkleme/",
+        defaults: new { controller = "BankaKasa", action = "BankaKasaEkleme" }
+        );
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasaEklemeSantiyeEft/",
+        defaults: new { controller = "BankaKasa", action = "BankaKasaEklemeSantiyeEft" }
+        );
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasaEklemeEft/",
+        defaults: new { controller = "BankaKasa", action = "BankaKasaEklemeEft" }
+        );
+    //GUNCELLE
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasaGuncelle/{bankakasaid?}",
+        defaults: new { controller = "BankaKasa", action = "BankaKasaGuncelle" }
+        );
+    //DETAY
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasaDetay/{bankakasaid?}",
+        defaults: new { controller = "BankaKasa", action = "BankaKasaDetay" }
+        );
+    //SÝL
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasaSil/{bankakasaid?}",
+        defaults: new { controller = "BankaKasa", action = "BankaKasaSil" }
+        );
+    app.MapControllerRoute(
+        name: "BankaKasa",
+        pattern: "BankaKasa/BankaKasaSilSantiyeEft/{bankakasasantiyeid?}",
+        defaults: new { controller = "BankaKasa", action = "BankaKasaSilSantiyeEft" }
+        );
+    #endregion
+
+    #region ÇEK
+    //INDEX
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/Index",
+        defaults: new { controller = "Cek", action = "Index" }
+        );
+    //EKLEME
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekEklemeFromCari/{carihesapid?}",
+        defaults: new { controller = "Cek", action = "CekEklemeFromCari" }
+        );
+    //TAHSÝL
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekTahsil/{cekid?}",
+        defaults: new { controller = "Cek", action = "CekTahsil" }
+        );
+    //GUNCELLE
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekGuncelle/{cekid?}",
+        defaults: new { controller = "Cek", action = "CekGuncelle" }
+        );
+    //DETAY
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekDetay/{cekid?}",
+        defaults: new { controller = "Cek", action = "CekDetay" }
+        );
+    //SÝL
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekSil/{cekid?}",
+        defaults: new { controller = "Cek", action = "CekSil" }
+        );
+    //FÝLTRE
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekSantiye/{santiyeid?}",
+        defaults: new { controller = "Cek", action = "CekSantiye" }
+        );
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekBanka/{bankahesapid?}",
+        defaults: new { controller = "Cek", action = "CekBanka" }
+        );
+    app.MapControllerRoute(
+        name: "Cek",
+        pattern: "Cek/CekSirket/{sirketid?}",
+        defaults: new { controller = "Cek", action = "CekSirket" }
+        );
+    #endregion
+
+    #region NAKÝT
+    //EKLEME
+    app.MapControllerRoute(
+        name: "Nakit",
+        pattern: "Nakit/NakitEklemeFromCari/{carihesapid?}",
+        defaults: new { controller = "Nakit", action = "NakitEklemeFromCari" }
+        );
+    //GÜNCELLE
+    app.MapControllerRoute(
+        name: "Nakit",
+        pattern: "Nakit/NakitGuncelle/{nakitid?}",
+        defaults: new { controller = "Nakit", action = "NakitGuncelle" }
+        );
+    //DETAY
+    app.MapControllerRoute(
+        name: "Nakit",
+        pattern: "Nakit/NakitDetay/{nakitid?}",
+        defaults: new { controller = "Nakit", action = "NakitDetay" }
+        );
+    //SÝL
+    app.MapControllerRoute(
+        name: "Nakit",
+        pattern: "Nakit/NakitSil/{nakitid?}",
+        defaults: new { controller = "Nakit", action = "NakitSil" }
+        );
+    //FÝLTRE
+    app.MapControllerRoute(
+        name: "Nakit",
+        pattern: "Nakit/NakitSantiye/{santiyeid?}",
+        defaults: new { controller = "Nakit", action = "NakitSantiye" }
+        );
+    app.MapControllerRoute(
+        name: "Nakit",
+        pattern: "Nakit/NakitBanka/{bankahesapid?}",
+        defaults: new { controller = "Nakit", action = "NakitBanka" }
+        );
+    app.MapControllerRoute(
+        name: "Nakit",
+        pattern: "Nakit/NakitSirket/{sirketid?}",
+        defaults: new { controller = "Nakit", action = "NakitSirket" }
+        );
+    #endregion
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=AnaSayfa}/{action=Index}/{id?}");
+
 
 app.Run();
